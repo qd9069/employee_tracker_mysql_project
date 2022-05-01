9bd281426f89bffa1568208d970ae99335de6fd1
Elaine Deng <qd9069@gmail.com>
2022-04-30 22:06:34 -0700

    Added query.sql for joining tables

--- db/query.sql
+++ db/query.sql
@@ -4,7 +4,16 @@
 -- JOIN <right_table>
 -- ON <left_table>.<column> = <right_table>.<column>;
 
+-- Join for viewing all roles
+SELECT role.id, role.title, department.name as department, role.salary
+FROM role
+JOIN department 
+ON role.department_id = department.id;
 
--- SELECT *
--- FROM course_names
--- JOIN department ON course_names.department = department.id;
+-- Join for viewing all employees
+SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name as department, role.salary, employee.manager_id as manager
+FROM employee
+JOIN role ON role_id = role.id
+JOIN department ON department_id = department.id;
+-- JOIN employee m ON employee.manager_id = employee.id
+
